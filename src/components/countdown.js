698825ba85c9f2abc5a6f71117e23f8b4335a59b
Kayro França <k4yro01@gmail.com>
2020-12-25 11:28:16 -0300

    added function calculateTimeLeft

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -3,12 +3,28 @@ import './countdown.css';
 
 
 function Countdown() {
-    return (
-      <div>
-        
-      </div>
-    );
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`${year}, 12, 31`) - +new Date();
+    let timeLeft = {};
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+    return timeLeft;
   }
+
+
+  return (
+    <div className="containerCountdown">
+      {calculateTimeLeft}
+    </div>
+  );
+}
   
-  export default Countdown;
+export default Countdown;
   
